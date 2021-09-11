5a05ea169ae5e3cd16ce055d7351d64441f0a320
Quentin Rasmont <qrasmont@gmail.com>
2021-09-11 20:47:03 +0200

    Make adding different themes a bit easier

--- lua/zenline/utils/colors.lua
+++ lua/zenline/utils/colors.lua
@@ -1,14 +1,6 @@
+
 -- Define some theme colors to be use to color the line
-local theme_colors = {
-    light = '#EBDBB2',
-    dark = '#3C3836',
-    black = '#1D2021',
-    grey = '#504945',
-    red = '#FB4934',
-    green = '#B8BB26',
-    orange = '#FE8019',
-    blue = '#83A598',
-}
+local theme_colors = require('zenline.themes.gruvbox').colors
 
 -- Color to Highlight group table
 local colors = {
